8f10ad387c95c9637a38dc2f36d953cfe227c028
Florian Suess <floriansuess96@icloud.com>
2024-09-06 18:39:13 +1200

    Remove the "you've been here for x"

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let seconds = 0;
-  const timeElement = document.getElementById("time");
+document.addEventListener('DOMContentLoaded', () => {
+  const diagnostics = document.getElementById('diagnostic-data');
+  const pollDiagnostics = async () => {
+    const response = await fetch('/diagnostics');
+    if (response.ok) {
+      const data = await response.json();
+      diagnostics.innerHTML = '';
 
-  setInterval(() => {
-    seconds++;
-    timeElement.textContent = `${seconds}s`;
-  }, 1000);
+      for (const [key, value] of Object.entries(data)) {
+        const latest = value[value.length - 1]
+        const p = document.createElement('p');
+        p.textContent = `${key}: ${latest.value}`;
+        diagnostics.appendChild(p);
+      }
+    } else {
+      console.error('error fetching diagnostics data');
+    }
+  };
+
+  setInterval(pollDiagnostics, 1000);
 });
