61d49db073150249346b8aa4e2eaccb892f6c257
Florian Suess <floriansuess96@icloud.com>
2024-09-27 17:06:02 +1200

    Generating for various merchants

--- gen.go
+++ gen.go
@@ -17,6 +17,7 @@ const (
 	DIAGNOSTIC_GENERATED_TRANSACTIONS      = "Generated transactions"
 	DIAGNOSTIC_GENERATED_TRANSACTION_LINES = "Generated transaction lines"
 	DIAGNOSTIC_GENERATED_PRODUCTS          = "Generated products"
+	DIAGNOSTIC_GENERATED_MERCHANTS         = "Generated merchants"
 )
 
 func generator(ctx context.Context, lg *logrus.Logger, connector *duckdb.Connector) error {
@@ -26,25 +27,91 @@ func generator(ctx context.Context, lg *logrus.Logger, connector *duckdb.Connect
 	}
 	defer conn.Close()
 
-	products, err := generateProducts(ctx, lg, conn, 20)
+	merchants, err := generateMerchants(ctx, lg, conn, 1)
 	if err != nil {
 		return err
 	}
 
-	transactions, err := generateTransactions(ctx, lg, conn, 10_000_000)
-	if err != nil {
-		return err
-	}
+	for _, merchant := range merchants {
+		products, err := generateProducts(ctx, lg, conn, merchant, rand.Int()%100)
+		if err != nil {
+			return err
+		}
 
-	err = generateTransactionLines(ctx, lg, conn, products, transactions, 100_000_000)
-	if err != nil {
-		return err
+		transactions, err := generateTransactions(ctx, lg, conn, merchant, rand.Int()%100_000)
+		if err != nil {
+			return err
+		}
+
+		err = generateTransactionLines(ctx, lg, conn, merchant, products, transactions, len(transactions)*7)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func generateProducts(ctx context.Context, lg *logrus.Logger, conn driver.Conn, amount int) ([]uuid.UUID, error) {
+func generateMerchants(ctx context.Context, lg *logrus.Logger, conn driver.Conn, amount int) ([]uuid.UUID, error) {
+	appender, err := duckdb.NewAppenderFromConn(conn, "", "merchants")
+	if err != nil {
+		return nil, fmt.Errorf("failed to establish appender for merchants: %w", err)
+	}
+	defer lg.WithField("quantity", amount).Debug("generated merchants flushed to disk")
+	defer appender.Close()
+
+	var names = []string{
+		"Tech",
+		"Spark",
+		"Volt",
+		"Nano",
+		"Sync",
+		"Proto",
+		"Quantum",
+		"Byte",
+		"Pulse",
+		"Gear",
+		"Hex",
+		"Vibe",
+		"Echo",
+		"Glide",
+		"Nex",
+		"Flex",
+		"Optic",
+		"Circuit",
+		"Zylo",
+		"Fusion",
+	}
+
+	var postfixes = []string{
+		"Co.",
+		"Ltd.",
+		"Inc.",
+		"Corporation",
+		"LLC",
+		"GmbH",
+		"Enterprises",
+		"Industries",
+		"Solutions",
+		"Group",
+	}
+
+	merchants := make([]uuid.UUID, amount)
+	for i := 0; i < amount; i++ {
+		merchants[i] = uuid.New()
+		if err := appender.AppendRow(
+			duckdb.UUID(merchants[i]),
+			names[rand.Int()%len(names)]+names[rand.Int()%len(names)]+" "+postfixes[rand.Int()%len(postfixes)],
+		); err != nil {
+			return nil, fmt.Errorf("failed to append merchant row: %w", err)
+		}
+		diagnostics.DiagnosticsFromContext(ctx).Set(DIAGNOSTIC_GENERATED_MERCHANTS, i+1)
+	}
+
+	return merchants, nil
+}
+
+func generateProducts(ctx context.Context, lg *logrus.Logger, conn driver.Conn, merchant uuid.UUID, amount int) ([]uuid.UUID, error) {
 	appender, err := duckdb.NewAppenderFromConn(conn, "", "products")
 	if err != nil {
 		return nil, fmt.Errorf("failed to establish appender for products: %w", err)
@@ -82,6 +149,7 @@ func generateProducts(ctx context.Context, lg *logrus.Logger, conn driver.Conn, 
 			duckdb.UUID(products[i]),
 			names[rand.Int()%len(names)]+" "+names[rand.Int()%len(names)],
 			rand.Int31()%10_000+100,
+			duckdb.UUID(merchant),
 		); err != nil {
 			return nil, fmt.Errorf("failed to append product row: %w", err)
 		}
@@ -91,7 +159,7 @@ func generateProducts(ctx context.Context, lg *logrus.Logger, conn driver.Conn, 
 	return products, nil
 }
 
-func generateTransactions(ctx context.Context, lg *logrus.Logger, conn driver.Conn, amount int) ([]uuid.UUID, error) {
+func generateTransactions(ctx context.Context, lg *logrus.Logger, conn driver.Conn, merchant uuid.UUID, amount int) ([]uuid.UUID, error) {
 	appender, err := duckdb.NewAppenderFromConn(conn, "", "transactions")
 	if err != nil {
 		return nil, fmt.Errorf("failed to establish appender for transactions: %w", err)
@@ -106,6 +174,7 @@ func generateTransactions(ctx context.Context, lg *logrus.Logger, conn driver.Co
 		if err := appender.AppendRow(
 			duckdb.UUID(transactions[i]),
 			now.Add(-time.Duration(rand.Int())*time.Hour),
+			duckdb.UUID(merchant),
 		); err != nil {
 			return nil, fmt.Errorf("failed to append transaction row: %w", err)
 		}
@@ -115,7 +184,7 @@ func generateTransactions(ctx context.Context, lg *logrus.Logger, conn driver.Co
 	return transactions, nil
 }
 
-func generateTransactionLines(ctx context.Context, lg *logrus.Logger, conn driver.Conn, products, transactions []uuid.UUID, amount int) error {
+func generateTransactionLines(ctx context.Context, lg *logrus.Logger, conn driver.Conn, merchant uuid.UUID, products, transactions []uuid.UUID, amount int) error {
 	appender, err := duckdb.NewAppenderFromConn(conn, "", "transaction_lines")
 	if err != nil {
 		return fmt.Errorf("failed to establish appender for transaction lines: %w", err)
@@ -128,6 +197,7 @@ func generateTransactionLines(ctx context.Context, lg *logrus.Logger, conn drive
 			duckdb.UUID(transactions[rand.Int()%len(transactions)]),
 			duckdb.UUID(products[rand.Int()%len(products)]),
 			int32(rand.Int()%13),
+			duckdb.UUID(merchant),
 		); err != nil {
 			return fmt.Errorf("failed to append transaction line row: %w", err)
 		}
--- store/duckdb/migrations/01_init.sql
+++ store/duckdb/migrations/01_init.sql
@@ -1,5 +1,24 @@
+CREATE TABLE IF NOT EXISTS main.merchants (
+  id UUID PRIMARY KEY,
+  name VARCHAR
+);
+
 CREATE TABLE IF NOT EXISTS main.products (
   id UUID PRIMARY KEY,
   name VARCHAR,
-  price_cents INTEGER
+  price_cents INTEGER,
+  merchant_id UUID REFERENCES main.merchants(id)
+);
+
+CREATE TABLE IF NOT EXISTS main.transactions (
+  id UUID PRIMARY KEY,
+  created_at TIMESTAMP,
+  merchant_id UUID REFERENCES main.merchants(id)
+);
+
+CREATE TABLE IF NOT EXISTS main.transaction_lines (
+  transaction_id UUID REFERENCES main.transactions(id),
+  product_id UUID REFERENCES main.products(id),
+  quantity INTEGER,
+  merchant_id UUID REFERENCES main.merchants(id)
 );
