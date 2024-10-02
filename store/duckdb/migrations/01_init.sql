d9e24fe8e2bb8d4ce5ecdb1651f0c693b1cc1082
Florian Suess <floriansuess96@icloud.com>
2024-10-02 18:42:33 +1300

    Drop constraints for speed

--- store/duckdb/migrations/01_init.sql
+++ store/duckdb/migrations/01_init.sql
@@ -1,25 +1,25 @@
 CREATE TABLE IF NOT EXISTS main.merchants (
-  id UUID PRIMARY KEY,
+  id UUID, -- PRIMARY KEY
   name VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS main.products (
-  id UUID PRIMARY KEY,
+  id UUID, -- PRIMARY KEY
   name VARCHAR,
   price_cents INTEGER,
-  merchant_id UUID REFERENCES main.merchants(id)
+  merchant_id UUID, -- REFERENCES main.merchants(id)
 );
 
 CREATE TABLE IF NOT EXISTS main.transactions (
-  id UUID PRIMARY KEY,
+  id UUID, -- PRIMARY KEY
   created_at TIMESTAMP,
-  merchant_id UUID REFERENCES main.merchants(id)
+  merchant_id UUID, -- REFERENCES main.merchants(id)
 );
 
 CREATE TABLE IF NOT EXISTS main.transaction_lines (
-  id UUID PRIMARY KEY,
-  transaction_id UUID REFERENCES main.transactions(id),
-  product_id UUID REFERENCES main.products(id),
+  id UUID, -- PRIMARY KEY,
+  transaction_id UUID, -- REFERENCES main.transactions(id)
+  product_id UUID, -- REFERENCES main.products(id)
   quantity INTEGER,
-  merchant_id UUID REFERENCES main.merchants(id)
+  merchant_id UUID, -- REFERENCES main.merchants(id)
 );
